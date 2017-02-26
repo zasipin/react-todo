cacd16d1268dd6a623d66acb0c7d8ec8fc1e1020
zasipin <zasipin.it@gmail.com>
2017-02-26 17:40:11 +0300

    Added Todo component, changed foundation settings and path in webpack

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TodoApp extends React.Component{
+  render() {
+    return (
+      <div>
+        TodoApp component
+      </div>
+    );
+  }
+}
