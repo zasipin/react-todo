2abde8cafd39fb4e6ab1a2d5218590184f2ddbc7
zasipin <zasipin.it@gmail.com>
2017-02-28 22:11:44 +0300

    Add search component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
+import TodoSearch from 'TodoSearch';
 
 export default class TodoApp extends React.Component{
   constructor(){
     super();
     this.state = {
+      showCompleted: false,
+      searchText: '',
       todos: [
         { id: 1, text: 'Walk the dog'},
         { id: 2, text: 'Clean the yard'},
@@ -18,11 +21,19 @@ export default class TodoApp extends React.Component{
     console.log(text);
   }
 
+  handleSearch(showCompleted, searchText){
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    })
+  }
+
   render() {
     var {todos} = this.state;
     return (
       <div>
         TodoApp component
+        <TodoSearch onSearch={(showCompleted, searchText) => this.handleSearch(showCompleted, searchText)} />
         <TodoList todos={todos} />
         <AddTodo onAddButtonClick={(text) => this.handleAddButton(text)} />
       </div>
---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class TodoSearch extends React.Component {
+  handleSearch(){
+    var showCompleted = this.refs.showCompleted.checked;
+    var searchText = this.refs.searchText.value;
+
+    this.props.onSearch(showCompleted, searchText);
+  }
+
+  render(){
+    return (
+      <div>
+        <div>
+          <input type="text" ref="searchText" placeholder="Search todos" onChange={()=>this.handleSearch()}/>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" ref="showCompleted" onChange={()=>this.handleSearch()}/>
+            Show completed todos
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
