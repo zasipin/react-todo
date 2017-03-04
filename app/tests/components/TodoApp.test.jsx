e7d6a48ce1956988d35054c5ccbd237c5da8939a
zasipin <zasipin.it@gmail.com>
2017-03-04 17:39:52 +0300

    Add tests for toggle todo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -21,4 +21,22 @@ describe('TodoApp', () => {
      expect(todoApp.state.todos.length).toBe(1);
      expect(todoApp.state.todos[0].text).toBe(todoText);
    });
+
+   it('should toggle completed value when handleToggle called', () => {
+     var todoData = {
+       id: 11,
+       text: 'test',
+       completed: false
+     }
+
+     var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+     todoApp.setState({todos: [todoData]});
+
+     expect(todoApp.state.todos[0].completed).toBe(false);
+
+     todoApp.handleToggle(11);
+
+     expect(todoApp.state.todos[0].completed).toBe(true);
+
+   });
 });
