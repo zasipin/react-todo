7381007b746d2609f38bd61ecde545a19d7a10dd
10-1-27-221-WV\ZasypinNV <ZasypinNV@yanos.slavneft.ru>
2017-03-17 12:55:10 +0300

    Add reducers step 2

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -5,3 +5,11 @@ export function searchTextReducer(state = '', action) {
         default: return state; 
     }
 }
+
+export function showCompletedReducer(state = false, action) {
+    switch (action.type){
+        case 'TOGGLE_SHOW_COMPLETED':
+            return !state;
+        default: return state; 
+    }
+}
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -3,14 +3,30 @@ import expect from 'expect';
 import * as reducers from 'reducers';
 
 describe('Reducers', () => {
-    it('shuold return search text on action searchText', () => {
-        var action = {
-            type: 'SET_SEARCH_TEXT',
-            searchText: 'Some search text'
-        }
+    describe('searchTextReducer', () => {
+        it('should return search text on action searchText', () => {
+            var action = {
+                type: 'SET_SEARCH_TEXT',
+                searchText: 'Some search text'
+            }
 
-        var res = reducers.searchTextReducer('', action);
+            var res = reducers.searchTextReducer('', action);
 
-        expect(res).toEqual(action.searchText);
+            expect(res).toEqual(action.searchText);
+        });
+    });
+
+    describe('showCompletedReducer', () => {
+        it('should return oppsite meaning on action ЕщппдуShowCompleted', () => {
+            var action = {
+                type: 'TOGGLE_SHOW_COMPLETED',
+            }
+
+            var res = reducers.showCompletedReducer(false, action);
+            expect(res).toEqual(true);
+
+            var res = reducers.showCompletedReducer(true, action);
+            expect(res).toEqual(false);
+        });
     });
 });
