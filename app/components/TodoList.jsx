19311e004942761a91fb085a78524ab066cbc346
zasipin <zasipin.it@gmail.com>
2017-04-02 22:14:43 +0300

    fetch todos from firebase and Updated TodosList

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -8,15 +8,17 @@ export class TodoList extends React.Component {
     var {todos, showCompleted, searchText} = this.props;
     var that = this;
     var renderTodoList = () => {
-      if (todos.length === 0) {
+      var filteredTodos = TodoApi.filterTodos(todos, showCompleted, searchText);
+
+      if (filteredTodos.length === 0) {
         return (
           <p className="container__message">Nothing to do</p>
         )
       }
 
-      return  TodoApi.filterTodos(todos, showCompleted, searchText).map((todo) => {
+      return  filteredTodos.map((todo) => {
         return <Todo key={todo.id} {...todo} />//onToggle={(id) => that.props.onToggle(id)} />
-      })
+      });
     }
 
     return (
