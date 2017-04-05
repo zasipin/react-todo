74567bfc4e5541fe95932c4d517914723cf8e060
zasipin <zasipin.it@gmail.com>
2017-04-05 19:22:42 +0300

    Add environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,11 +3,12 @@ import firebase from 'firebase';
 try {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyA8GNkGaJWx--TyJUlNyPBL2L-5fcyMdt8",
-    authDomain: "reactclass-todoapp.firebaseapp.com",
-    databaseURL: "https://reactclass-todoapp.firebaseio.com",
-    storageBucket: "reactclass-todoapp.appspot.com",
-    messagingSenderId: "533369736188"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
   firebase.initializeApp(config); 
 } catch(e) {
